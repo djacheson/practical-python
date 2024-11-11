66d38c56c460299443961d303f5963d85673b995
djacheson <djacheson@gmail.com>
2024-11-11 14:43:36 -0600

    first couple exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+start_height = 100
+for i in range(11):
+    if i == 0:
+        curr_height = start_height
+    else:
+        curr_height = curr_height * (3/5)
+        print(f"{i} {round(curr_height, 4)}")
